643b70b7f601a58141a113c3cb8abfd250b5fdb7
Dinesh <djeeva1515@gmail.com>
2023-11-15 13:30:51 +0530

    home page changes

--- src/pages/Home/Homepage.tsx
+++ src/pages/Home/Homepage.tsx
@@ -1,24 +1,13 @@
 import Banner from "../../assets/BeStore-banner.jpg";
 import { Box } from "@mui/material";
+import SignUpNewsLetter from "../Sign-Up/SignUp";
 
 export default function Homepage() {
   return (
     <Box>
       <img src={Banner} style={{ maxWidth: "1519px", height: "100%" }} alt="" />
-      {/* <Box textAlign="center" mt={2}>
-        <Typography variant="h1">Shoping as you love</Typography>
-        <Typography variant="subtitle1" color="text.secondary">
-          Explore a wide range of products to suit your style.
-        </Typography>
-        <Button
-          variant="contained"
-          color="primary"
-          size="large"
-          style={{ marginTop: "1rem" }}
-        >
-          Shop Now
-        </Button>
-      </Box> */}
+
+      <SignUpNewsLetter />
     </Box>
   );
 }
