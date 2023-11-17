79212b3f25a85cbea822a76dd04a5bde455134ac
suryaKanthjunior <114057966+suryaKanthjunior@users.noreply.github.com>
2023-11-17 18:26:11 +0530

    comitted

--- src/pages/Contact/Contactpage.tsx
+++ src/pages/Contact/Contactpage.tsx
@@ -1,9 +1,31 @@
-import { Box } from "@mui/material";
+import { Box, Divider, Typography } from "@mui/material";
+import EmailIcon from '@mui/icons-material/Email';
+import CallIcon from '@mui/icons-material/Call';
 
 export default function ContactPage() {
   return (
-    <Box>
-      <h1>Welcome to the contact page</h1>
+    <>
+    <Box marginTop={"150px"}>
+     <Typography  lineHeight={"1"} textAlign={"center"} color={"black"} fontWeight={"400"} fontSize={"70px"} fontFamily={"sans-serif"}>Lorem ipsum dolor mit</Typography>
+     <Typography  lineHeight={"1"} textAlign={"center"} color={"black"} fontWeight={"400"} fontSize={"70px"} fontFamily={"sans-serif"}>samet et omnia</Typography>
     </Box>
+    <Box display={"flex"} justifyContent={"center"}>
+    <Box width={"900px"} height={"150px"} border={"1px solid lightgray"} marginTop={"90px"} display={"flex"}>
+    <Box textAlign={"center"}>
+      <EmailIcon style={{
+        fontSize: "32px"
+      }} />
+      <Typography color={"black"} fontSize={"25px"}>+61 (0) 7 9180 3458</Typography>
+      </Box>
+      <Divider orientation="vertical" flexItem />
+      <Box textAlign={"center"}>
+        <CallIcon style={{
+        fontSize: "32px"
+      }} />
+        <Typography color={"pink"} fontSize={"25px"}>noreply@envato.com</Typography>
+      </Box>
+      </Box>
+    </Box>
+    </>
   );
 }
