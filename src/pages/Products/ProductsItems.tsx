260752740f40e0138e4903a17338de293b0da7d7
Dinesh <djeeva1515@gmail.com>
2023-11-20 18:25:06 +0530

    products page items added

---
+++ src/pages/Products/ProductsItems.tsx
@@ -0,0 +1,144 @@
+import Bag from "../../assets/ProductsImage/bag.jpg";
+import Coat from "../../assets/ProductsImage/dress.jpg";
+import Creame from "../../assets/ProductsImage/cream.jpg";
+import Dress from "../../assets/ProductsImage/dress2.jpg";
+import Facebalm from "../../assets/ProductsImage/face balm.jpg";
+import FlipFlops from "../../assets/ProductsImage/flipflops.jpg";
+import Glasses from "../../assets/ProductsImage/Glasses.jpg";
+import Goldchan from "../../assets/ProductsImage/Gold chain.jpg";
+import Jacket from "../../assets/ProductsImage/Products.-best2.jpg";
+import Lipsstick from "../../assets/ProductsImage/lipsstick.jpg";
+import Necklace from "../../assets/ProductsImage/Necklace.jpg";
+import Pants from "../../assets/ProductsImage/Products.-bestjpg.jpg";
+import Redshoes from "../../assets/ProductsImage/red shoes.jpg";
+import Shades from "../../assets/ProductsImage/shades.jpg";
+import Shoes from "../../assets/ProductsImage/shoe.jpg";
+import Soap from "../../assets/ProductsImage/soap.jpg";
+import Sweater from "../../assets/ProductsImage/sweater.jpg";
+import Swimsuit from "../../assets/ProductsImage/swimsuit.jpg";
+import Watch from "../../assets/ProductsImage/watch.jpg";
+import shirts from "../../assets/ProductsImage/shirts.jpg";
+
+const ProductsImage = [
+  {
+    name: "Bag",
+    Image: Bag,
+    price: 156.0,
+    type: "not-best selling",
+  },
+  {
+    name: "Beige shoes",
+    Image: Shoes,
+    price: 189.0,
+    type: "Best selling",
+  },
+  {
+    name: "Coat",
+    Image: Coat,
+    price: 600.0,
+    type: "Best selling",
+  },
+  {
+    name: "Creame",
+    Image: Creame,
+    price: 8.0,
+    type: "Best selling",
+  },
+  {
+    name: "Dress",
+    Image: Dress,
+    price: 99.0,
+    type: "Best selling",
+  },
+  {
+    name: "Face Balm",
+    Image: Facebalm,
+    price: 9.0,
+    type: "Best selling",
+  },
+  {
+    name: "Flip flops",
+    Image: FlipFlops,
+    price: 0,
+    type: "not-Best selling",
+  },
+  {
+    name: "Glasses",
+    Image: Glasses,
+    price: 99.0,
+    type: "not-Best selling",
+  },
+  {
+    name: "Gold chain",
+    Image: Goldchan,
+    price: 899.0,
+    type: "not-Best selling",
+  },
+  {
+    name: "Jacket",
+    Image: Jacket,
+    price: 299.0,
+    type: "not-Best selling",
+  },
+  {
+    name: "Lipsstick",
+    Image: Lipsstick,
+    price: 19.0,
+    type: "not-Best selling",
+  },
+  {
+    name: "Necklace",
+    Image: Necklace,
+    price: 199.0,
+    type: "not-Best selling",
+  },
+  {
+    name: "Pants",
+    Image: Pants,
+    price: 55.0,
+    type: "Best selling",
+  },
+  {
+    name: "Red shoes",
+    Image: Redshoes,
+    price: 89.0,
+    type: "not-Best selling",
+  },
+  {
+    name: "Shades",
+    Image: Shades,
+    price: 59.0,
+    type: "not-Best selling",
+  },
+  {
+    name: "Shirts",
+    Image: shirts,
+    price: 79.0,
+    type: "not-Best selling",
+  },
+  {
+    name: "Soap",
+    Image: Soap,
+    price: 5.0,
+    type: "not-Best selling",
+  },
+  {
+    name: "Sweater",
+    Image: Sweater,
+    price: 79.0,
+    type: "Best selling",
+  },
+  {
+    name: "Swimsuit",
+    Image: Swimsuit,
+    price: 49.0,
+    type: "not-Best selling",
+  },
+  {
+    name: "Watch",
+    Image: Watch,
+    price: 499.0,
+    type: "not-Best selling",
+  },
+];
+export default ProductsImage;
--- src/pages/Products/Productspage.tsx
+++ src/pages/Products/Productspage.tsx
@@ -1,13 +1,93 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
+
+import BadgeStyle from "../Home/HomepageProducts/BadgeStyle";
+import CurrencyPoundIcon from "@mui/icons-material/CurrencyPound";
+import ProductsHeader from "./ProductsHeader";
+import ProductsImage from "./ProductsItems";
 
 export default function ProductsPage() {
+  const TotalItems = ProductsImage.length;
+
   return (
     <Box>
-      <Box padding={"175px 0 60px"} textAlign={"center"} bgcolor={"#F6F6F6"}>
-        <Typography fontSize={"80px"} fontWeight={"bold"}>
+      <Box padding={"185px 0 60px"} textAlign={"center"} bgcolor={"#F6F6F6"}>
+        <Typography fontSize={"75px"} fontWeight={"bold"}>
           {" "}
           Products
         </Typography>
+      </Box>
+      <Box bgcolor={"white"} width={"100%"} margin={"0 auto"}>
+        <Box
+          margin={"0 auto"}
+          maxWidth={1160}
+          pt={"30px"}
+          borderRight={"1px solid lightgray"}
+        >
+          <ProductsHeader totalItems={TotalItems} />
+
+          <Box mb={15}>
+            <Grid container>
+              {ProductsImage.map((item, index) => (
+                <Grid columnSpacing={"2rem"} item xs={12} sm={12} md={4}>
+                  <Box
+                    mt={2}
+                    maxWidth={260}
+                    width={"100%"}
+                    gap={"2rem"}
+                    key={index}
+                    position="relative"
+                    style={{ overflow: "hidden", cursor: "pointer" }}
+                  >
+                    <img
+                      style={{
+                        height: 260,
+                        maxWidth: 260,
+                        width: "100%",
+                        transition: "transform 0.5s ease",
+                      }}
+                      onMouseEnter={(e) => {
+                        e.currentTarget.style.transform = "scale(1.1)";
+                      }}
+                      onMouseLeave={(e) => {
+                        e.currentTarget.style.transform = "scale(1)";
+                      }}
+                      src={item.Image}
+                      alt="products"
+                    />
+                    {item.type === "Best selling" && <BadgeStyle />}
+                    <Box
+                      display={"flex"}
+                      flexDirection={"column"}
+                      justifyContent={"flex-start"}
+                    >
+                      <Typography
+                        textAlign={"left"}
+                        fontSize={"21px"}
+                        fontWeight={"bold"}
+                        mt={1}
+                      >
+                        {item.name}
+                      </Typography>
+                      <Box display={"flex"}>
+                        {item.price ? (
+                          <CurrencyPoundIcon style={{ color: "#EB3C70" }} />
+                        ) : null}
+
+                        <Typography
+                          fontSize={"18px"}
+                          fontWeight={"bold"}
+                          color={"#EB3C70"}
+                        >
+                          {item.price}.00
+                        </Typography>
+                      </Box>
+                    </Box>
+                  </Box>
+                </Grid>
+              ))}
+            </Grid>
+          </Box>
+        </Box>
       </Box>
     </Box>
   );
