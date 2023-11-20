b1f7d21f363aa5aa9019b332145bb4b9c010450a
Dinesh <djeeva1515@gmail.com>
2023-11-20 10:50:27 +0530

    customer ratings added animated

---
+++ src/pages/Home/CustomerRatings/CustomerRatings.tsx
@@ -0,0 +1,72 @@
+import { Box, Stack, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+
+import CustomerViews from "../../../assets/customer-views-bg.png";
+
+export default function CustomerTarings() {
+  const [count, setCount] = useState(1);
+  const [count2, setCount2] = useState(1);
+  const [count3, setCount3] = useState(1);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (count < 130) {
+        setCount((prevCount: any) => prevCount + 1);
+      }
+      if (count2 < 4) {
+        setCount2((prevCount: any) => prevCount + 1);
+      }
+      if (count3 < 10) {
+        setCount3((prevCount: any) => prevCount + 1);
+      }
+    }, 5);
+
+    return () => clearInterval(intervalId);
+  }, [count, count2, count3]);
+  return (
+    <Box>
+      <Box
+        sx={{
+          backgroundImage: `url(${CustomerViews})`,
+          backgroundSize: "contain",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          height: "100%",
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          paddingTop: "100px",
+          paddingBottom: "80px",
+        }}
+      >
+        <Stack
+          maxWidth={1140}
+          margin={"0 auto"}
+          width={"70%"}
+          justifyContent={"space-between"}
+          direction={"row"}
+        >
+          <Box textAlign={"center"}>
+            <Typography fontSize={"69px"} fontWeight={"bold"}>
+              {count2}M
+            </Typography>
+            <Typography fontSize={"15px"}>Lorem ipsum dolor</Typography>
+          </Box>{" "}
+          <Box textAlign={"center"}>
+            <Typography fontSize={"69px"} fontWeight={"bold"}>
+              {count}K
+            </Typography>
+            <Typography fontSize={"15px"}>Sid es omnia es</Typography>
+          </Box>
+          <Box textAlign={"center"}>
+            <Typography fontSize={"69px"} fontWeight={"bold"}>
+              {count3}
+            </Typography>
+            <Typography fontSize={"15px"}>Quanta fermium damus</Typography>
+          </Box>
+        </Stack>
+      </Box>
+    </Box>
+  );
+}
