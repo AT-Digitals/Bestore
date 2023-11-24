906feda7047c9749d72001a922de0b24309c989f
Dinesh <djeeva1515@gmail.com>
2023-11-24 18:56:24 +0530

    box shadow

--- src/pages/Contact/Contactpage.tsx
+++ src/pages/Contact/Contactpage.tsx
@@ -6,34 +6,41 @@ export default function ContactPage() {
   return (
     <Stack mt={"5rem"} direction={"row"}>
       <Box
+        width={"90%"}
         margin={"0 auto"}
-        maxWidth={600}
-        width={"100%"}
-        sx={{
-          backgroundColor: "white",
-        }}
+        boxShadow={"0px 1px 10px 2px"}
+        display={"flex"}
       >
-        <Typography
-          paddingTop={"80px"}
-          textAlign={"start"}
-          fontWeight={"600"}
-          fontSize={"px"}
-          color={"red"}
-          fontFamily={"sans-serif"}
+        <Box
+          margin={"0 auto"}
+          maxWidth={600}
+          width={"100%"}
+          sx={{
+            backgroundColor: "white",
+          }}
         >
-          INFORMATION
-        </Typography>
-        <Typography
-          textAlign={"start"}
-          fontWeight={"600"}
-          fontSize={"50px"}
-          color={"black"}
-          fontFamily={"sans-serif"}
-        >
-          Contact Us
-        </Typography>
+          <Typography
+            paddingTop={"80px"}
+            textAlign={"start"}
+            fontWeight={"600"}
+            fontSize={"px"}
+            color={"red"}
+            fontFamily={"sans-serif"}
+          >
+            INFORMATION
+          </Typography>
+          <Typography
+            textAlign={"start"}
+            fontWeight={"600"}
+            fontSize={"50px"}
+            color={"black"}
+            fontFamily={"sans-serif"}
+          >
+            Contact Us
+          </Typography>
+        </Box>
+        <ContactForm />
       </Box>
-      <ContactForm />
     </Stack>
   );
 }
