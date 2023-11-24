e428cc188af5e66c3b3ac05ce742f188d8ef6347
Dinesh <djeeva1515@gmail.com>
2023-11-24 18:50:07 +0530

    contact changes

--- src/pages/Contact/Contactpage.tsx
+++ src/pages/Contact/Contactpage.tsx
@@ -1,11 +1,39 @@
-import { Box } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
+
 import ContactForm from "./ContactForm";
-import map from "../../assets/store2-contact-map.png";
 
 export default function ContactPage() {
   return (
-    <>
+    <Stack mt={"5rem"} direction={"row"}>
+      <Box
+        margin={"0 auto"}
+        maxWidth={600}
+        width={"100%"}
+        sx={{
+          backgroundColor: "white",
+        }}
+      >
+        <Typography
+          paddingTop={"80px"}
+          textAlign={"start"}
+          fontWeight={"600"}
+          fontSize={"px"}
+          color={"red"}
+          fontFamily={"sans-serif"}
+        >
+          INFORMATION
+        </Typography>
+        <Typography
+          textAlign={"start"}
+          fontWeight={"600"}
+          fontSize={"50px"}
+          color={"black"}
+          fontFamily={"sans-serif"}
+        >
+          Contact Us
+        </Typography>
+      </Box>
       <ContactForm />
-    </>
+    </Stack>
   );
 }
