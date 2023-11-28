a7ab32ca1ea110f26fd31e048ad56ac052d27e75
suryaKanthjunior <114057966+suryaKanthjunior@users.noreply.github.com>
2023-11-28 13:08:16 +0530

    committed

--- src/pages/Home/AboutContainer/AboutContainer.tsx
+++ src/pages/Home/AboutContainer/AboutContainer.tsx
@@ -13,7 +13,7 @@ export default function AboutConainer() {
       margin={"0 auto"}
       display={"flex"}
       justifyContent={"space-between"}
-      padding={{ xs: "30px", sm: "0 40px" }}
+      padding={{ xs: "30px"}}
       gap={"3rem"}
     >
       <Box margin={"0 auto"}>
@@ -34,22 +34,22 @@ export default function AboutConainer() {
           fontSize={{ xs: "22px", md: "37px" }}
           fontFamily={"Nunito Sans, sans-serif"}
         >
-          We provide the newest trends in fashion
+          The story of AKKA CREATION in Mamallapuram
         </Typography>
         <Typography fontSize={"15px"} fontFamily={"Nunito Sans, sans-serif"}>
           Akka! <br />
-          The power of colors! India's fashion for women sure of them and full
-          of joie de vivre. Made by women for women up to size 54 or over .....
-          Sensual, colorful and of high quality. Each piece is unique and the
-          AKKA look is recognizable. Fashion Mamallapuram, a small town in the
-          Bay of Bengal that belongs to the world cultural heritage. In 2005,
+          The power of colors! <br /> India's fashion for women sure of them and full
+          of joie de vivre. <br /> Made by women for women up to size 54 or over .....
+          <br /> Sensual, colorful and of high quality. Each piece is unique and the
+          AKKA look is <br /> recognizable. <br /> Fashion Mamallapuram, a small town in the
+          Bay of Bengal that belongs to the world cultural heritage. <br /> In 2005,
           after the devastating tsunami, Véronique Thomassin had the idea to
           make fashion and accessories there and thus provide training for women
           and girls in the village, create jobs and allow better living
-          conditions An idea that has been successfully developed and which,
+          conditions <br /> An idea that has been successfully developed and which,
           thanks to the commitment of women, spreads the beauty of the colors of
-          India everywhere in the world Immerse yourself in this world full of
-          colors and joie de vivre. We welcome you on our site, and also in our
+          India everywhere in the world <br /> Immerse yourself in this world full of
+          colors and joie de vivre. <br /> We welcome you on our site, and also in our
           shop and our workshop in Mamallapuram.
         </Typography>
         <Link to={routes.ABOUT}>
