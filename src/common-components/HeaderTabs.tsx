f546838387be256e40eb10b9170aab7c98f1f2f9
Dinesh <djeeva1515@gmail.com>
2023-11-29 12:13:43 +0530

    scroll issues fixed

--- src/common-components/HeaderTabs.tsx
+++ src/common-components/HeaderTabs.tsx
@@ -1,7 +1,8 @@
-import { Box, Tab, Tabs } from "@mui/material";
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Box, Tab, Tabs, css } from "@mui/material";
+import { Link, useLocation } from "react-router-dom";
 import React, { useEffect } from "react";
 
+import logo from "./Akka creartions horizontal 2-03-03.png";
 import routes from "../routes/routes";
 import { useState } from "react";
 
@@ -12,17 +13,45 @@ const CustomTabs = [
   { name: "PRODUCTS", link: routes.PRODUCTS },
   { name: "CONTACT US", link: routes.CONTACT },
 ];
+const rotateKeyframes = css`
+  @keyframes rotate {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+  @-webkit-keyframes rotate {
+    from {
+      -webkit-transform: rotate(0deg);
+    }
+    to {
+      -webkit-transform: rotate(360deg);
+    }
+  }
+`;
 
 export default function HeaderTabs() {
   const [value, setValue] = useState(0);
-  const navigate = useNavigate();
   const location = useLocation();
+  const [loading, setLoading] = useState(false);
 
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setLoading(true);
     setValue(newValue);
-    navigate(CustomTabs[newValue].link);
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [loading]);
 
   useEffect(() => {
     const activeTab = CustomTabs.findIndex(
@@ -57,6 +86,33 @@ export default function HeaderTabs() {
           />
         ))}
       </Tabs>
+      {loading && (
+        <Box
+          position="fixed"
+          top={0}
+          left={0}
+          width="100%"
+          height="100%"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          sx={{
+            backgroundColor: "rgba(255, 255, 255, 0.7)",
+            zIndex: 1000,
+            backdropFilter: "blur(5px)",
+          }}
+        >
+          <img
+            src={logo}
+            alt=""
+            style={{
+              maxWidth: 160,
+              width: "100%",
+              animation: `${rotateKeyframes}rotate 2s linear infinite`,
+            }}
+          />
+        </Box>
+      )}
     </Box>
   );
 }
