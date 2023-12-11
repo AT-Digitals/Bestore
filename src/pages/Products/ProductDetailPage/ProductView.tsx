2a3102c2644f25ee10ec175d0ab5cce72c373989
Dinesh <djeeva1515@gmail.com>
2023-12-11 17:29:08 +0530

    product-ui-detail page change

---
+++ src/pages/Products/ProductDetailPage/ProductView.tsx
@@ -0,0 +1,161 @@
+import { IconButton, Stack } from "@mui/material";
+
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import Box from "@mui/material/Box";
+import React from "react";
+import Typography from "@mui/material/Typography";
+
+interface ProductSelectProps {
+  product: any;
+  handleGoBack: any;
+  relatedProducts: any[];
+}
+export default function ProductViewPage({
+  product,
+  handleGoBack,
+  relatedProducts,
+}: ProductSelectProps) {
+  //   const navigate = useNavigate();
+
+  //   const handleGoBack = () => {
+  //     navigate(-1);
+  //   };
+
+  const limitedRelatedProducts = relatedProducts.slice(0, 3);
+
+  return (
+    <Box
+      bgcolor={"white"}
+      justifyContent={"center"}
+      maxWidth={1519}
+      width={"100%"}
+    >
+      <Stack pb={5}>
+        <Box display={"flex"} alignItems={"center"} justifyContent={"left"}>
+          <IconButton onClick={handleGoBack}>
+            <ArrowBackIcon />
+          </IconButton>{" "}
+          <Typography> Go back</Typography>{" "}
+        </Box>
+
+        <Stack
+          padding={"60px"}
+          margin={"0px auto"}
+          spacing={8}
+          bgcolor={"white"}
+          boxShadow={"0px 0px 15px 0px"}
+          maxWidth={800}
+          direction={"row"}
+          mb={3}
+          width={"100%"}
+        >
+          <Box>
+            <img
+              src={product.Image}
+              alt=""
+              style={{
+                maxWidth: 350,
+                width: "100%",
+                height: "70%",
+                minHeight: 250,
+              }}
+            />
+          </Box>
+          <Stack spacing={3} maxWidth={400}>
+            <Typography
+              fontFamily={"Nunito Sans, sans-serif"}
+              fontSize={"20px"}
+              gutterBottom
+            >
+              {product.name}
+            </Typography>
+
+            <Typography
+              fontFamily={"Nunito Sans, sans-serif"}
+              variant="h6"
+              color="textSecondary"
+              gutterBottom
+            >
+              Category: {product.producttype}
+            </Typography>
+
+            <Typography fontFamily={"Nunito Sans, sans-serif"}>
+              Coat with quilted lining and an adjustable hood. Featuring long
+              sleeves with adjustable cuff tabs, adjustable asymmetric hem with
+              elastic side tabs and a front zip fastening with placket.
+            </Typography>
+          </Stack>
+        </Stack>
+
+        <Stack margin={"0 auto"}>
+          <Typography textAlign={"center"} fontSize={"30px"}>
+            Related Products
+          </Typography>
+          <Stack
+            paddingTop={3}
+            mt={10}
+            spacing={4}
+            direction={"row"}
+            width={"70%"}
+            margin={"0 auto"}
+          >
+            {limitedRelatedProducts.map((relatedProduct, index) => (
+              <Box
+                height={300}
+                width={330}
+                mb={2}
+                mt={2}
+                maxWidth={{ xs: 330, sm: 240, md: 330 }}
+                gap={"2rem"}
+                key={index}
+                position="relative"
+                style={{
+                  overflow: "hidden",
+                  cursor: "pointer",
+                  boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.2)",
+                  transition: "transform 0.5s ease",
+                }}
+                onMouseEnter={(e) => {
+                  e.currentTarget.style.transform = "scale(1.1)";
+                }}
+                onMouseLeave={(e) => {
+                  e.currentTarget.style.transform = "scale(1)";
+                }}
+                padding={"10px"}
+              >
+                <img
+                  style={{
+                    maxWidth: 330,
+                    width: "100%",
+                    minHeight: 250,
+                    objectFit: "cover",
+                    height: "70%",
+                  }}
+                  src={relatedProduct.Image}
+                  alt="products"
+                />
+
+                <Box
+                  display={"flex"}
+                  flexDirection={"column"}
+                  justifyContent={"flex-start"}
+                >
+                  <Typography
+                    fontSize={"10px"}
+                    padding={"5px"}
+                    textAlign={"center"}
+                    fontWeight={"bold"}
+                    mt={1}
+                    fontFamily={"Nunito Sans, sans-serif"}
+                  >
+                    {relatedProduct.name}
+                  </Typography>
+                </Box>
+              </Box>
+            ))}
+          </Stack>
+        </Stack>
+      </Stack>
+    </Box>
+  );
+}
--- src/pages/Products/ProductsCategories/ClothingProducts.tsx
+++ src/pages/Products/ProductsCategories/ClothingProducts.tsx
@@ -1,10 +1,19 @@
 import { Box, Grid, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 
-import { Link } from "react-router-dom";
+import ProductViewPage from "../ProductDetailPage/ProductView";
+import ProductsImage from "../ProductsItems";
 
 interface ProductProps {
   currentItems: any;
+}
+
+interface Product {
+  id: string;
+  Image: string;
+  name: string;
+  producttype: string;
+  // Add other properties as needed
 }
 export default function ClothingCategories({ currentItems }: ProductProps) {
   const [opacity, setOpacity] = useState(0);
@@ -12,86 +21,111 @@ export default function ClothingCategories({ currentItems }: ProductProps) {
   useEffect(() => {
     setOpacity(1.5);
   }, []);
+
+  const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
+
+  const handleProductClick = (product: any) => {
+    setSelectedProduct(product);
+  };
+
+  const relatedProducts = selectedProduct
+    ? ProductsImage.filter(
+        (item) =>
+          item.producttype === selectedProduct.producttype &&
+          item.id !== selectedProduct.id
+      )
+    : [];
   return (
-    <Box
-      style={{
-        opacity,
-        transition: "opacity 2s ease",
-        overflow: "hidden",
-        cursor: "pointer",
-      }}
-      mb={15}
-    >
-      <Grid marginLeft={"20px"} marginTop={"10px"} container>
-        {currentItems.map((item: any, index: any) => (
-          <Grid
-            columnSpacing={"2rem"}
-            rowGap={"4rem"}
-            item
-            xs={12}
-            sm={6}
-            md={4}
-          >
-            <Link
-              to={`/products/${item.id}`}
-              style={{
-                textDecoration: "none",
-                color: "black",
-                width: "100%",
-              }}
-            >
-              <Box
-                padding={"10px"}
-                height={360}
-                mb={2}
-                mt={2}
-                maxWidth={{ xs: 330, sm: 240, md: 320 }}
-                width={330}
-                gap={"2rem"}
-                key={index}
-                position="relative"
-                style={{
-                  overflow: "hidden",
-                  cursor: "pointer",
-                  boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.2)",
-                  transition: "transform 0.5s ease",
-                }}
-                onMouseEnter={(e) => {
-                  e.currentTarget.style.transform = "scale(1.1)";
-                }}
-                onMouseLeave={(e) => {
-                  e.currentTarget.style.transform = "scale(1)";
-                }}
+    <Box>
+      {selectedProduct ? (
+        <ProductViewPage
+          product={selectedProduct}
+          handleGoBack={() => setSelectedProduct(null)}
+          relatedProducts={relatedProducts}
+        />
+      ) : (
+        <Box
+          style={{
+            opacity,
+            transition: "opacity 2s ease",
+            overflow: "hidden",
+            cursor: "pointer",
+          }}
+          mb={15}
+        >
+          <Grid marginLeft={"20px"} marginTop={"10px"} container>
+            {currentItems.map((item: any, index: any) => (
+              <Grid
+                columnSpacing={"2rem"}
+                rowGap={"4rem"}
+                item
+                xs={12}
+                sm={6}
+                md={4}
               >
-                <img
-                  style={{
-                    height: "70%",
-                    maxWidth: "100%",
-                    width: "100%",
-                    objectFit: "cover",
-                  }}
-                  src={item.Image}
-                  alt="products"
-                />
+                {/* <Link
+                to={`/products/${item.id}`}
+                style={{
+                  textDecoration: "none",
+                  color: "black",
+                  width: "100%",
+                }}
+              > */}
                 <Box
-                  display={"flex"}
-                  flexDirection={"column"}
-                  justifyContent={"flex-start"}
+                  onClick={() => handleProductClick(item)}
+                  padding={"10px"}
+                  height={360}
+                  mb={2}
+                  mt={2}
+                  maxWidth={{ xs: 330, sm: 240, md: 320 }}
+                  width={330}
+                  gap={"2rem"}
+                  key={index}
+                  position="relative"
+                  style={{
+                    overflow: "hidden",
+                    cursor: "pointer",
+                    boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.2)",
+                    transition: "transform 0.5s ease",
+                  }}
+                  onMouseEnter={(e) => {
+                    e.currentTarget.style.transform = "scale(1.1)";
+                  }}
+                  onMouseLeave={(e) => {
+                    e.currentTarget.style.transform = "scale(1)";
+                  }}
                 >
-                  <Typography
-                    textAlign={"center"}
-                    fontSize={"10px"}
-                    fontWeight={"bold"}
-                    mt={1}
+                  <img
+                    style={{
+                      height: "70%",
+                      maxWidth: "100%",
+                      width: "100%",
+                      objectFit: "cover",
+                    }}
+                    src={item.Image}
+                    alt="products"
+                  />
+                  <Box
+                    display={"flex"}
+                    flexDirection={"column"}
+                    justifyContent={"flex-start"}
                   >
-                    {item.name}
-                  </Typography>
+                    <Typography
+                      textAlign={"center"}
+                      fontSize={"10px"}
+                      fontWeight={"bold"}
+                      mt={1}
+                    >
+                      {item.name}
+                    </Typography>
+                  </Box>
                 </Box>
-              </Box>
-            </Link>
+                {/* </Link> */}
+              </Grid>
+            ))}
           </Grid>
-        ))}
-      </Grid>
+        </Box>
+      )}
     </Box>
   );
 }
