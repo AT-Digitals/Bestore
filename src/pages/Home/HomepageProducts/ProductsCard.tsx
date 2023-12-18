d0acc9870054bc756a0f5aff0fef16038d7fcf5b
Dinesh <djeeva1515@gmail.com>
2023-12-18 11:21:20 +0530

    width changes fixed

--- src/pages/Home/HomepageProducts/ProductsCard.tsx
+++ src/pages/Home/HomepageProducts/ProductsCard.tsx
@@ -14,57 +14,63 @@ export default function ProductsCard({ relatedProduct }: Prosp) {
 
   return (
     <Stack direction={{ xs: "column", sm: "row", md: "row" }}>
-      <Grid padding={"0 20px"} ml={0} container spacing={2}>
+      <Grid
+        width={"100%"}
+        justifyContent={"center"}
+        ml={0}
+        container
+        spacing={2}
+      >
         {displayedProducts.map((item, index) => (
-          <Grid item xs={12} sm={6} md={2.4} key={index}>
-            <Link
+          <Grid
+            width={"100%"}
+            maxWidth={330}
+            item
+            xs={12}
+            sm={6}
+            md={2.4}
+            key={index}
+          >
+            <Box
+              borderRadius={"20px"}
+              height={"100%"}
+              mb={2}
+              key={index}
+              position="relative"
+              maxWidth={{ xs: 300, sm: 330, md: 280 }}
+              width={"100%"}
               style={{
-                margin: "0 auto",
-                textDecoration: "none",
-                color: "black",
+                overflow: "hidden",
+                cursor: "pointer",
+                transition: "transform 0.5s ease",
+                boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.2)",
               }}
-              to={""}
+              onMouseEnter={(e) => {
+                e.currentTarget.style.transform = "scale(1.1)";
+              }}
+              onMouseLeave={(e) => {
+                e.currentTarget.style.transform = "scale(1)";
+              }}
             >
-              <Box
-                borderRadius={"20px"}
-                height={"100%"}
-                mb={2}
-                key={index}
-                position="relative"
-                maxWidth={{ xs: 300, sm: 330, md: 280 }}
+              <img
                 style={{
-                  overflow: "hidden",
-                  cursor: "pointer",
-                  transition: "transform 0.5s ease",
-                  boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.2)",
+                  height: 260,
+                  maxWidth: 280,
+                  width: "100%",
                 }}
-                onMouseEnter={(e) => {
-                  e.currentTarget.style.transform = "scale(1.1)";
-                }}
-                onMouseLeave={(e) => {
-                  e.currentTarget.style.transform = "scale(1)";
-                }}
+                src={item.Image}
+                alt="products"
+              />
+              <Typography
+                fontSize={"13px"}
+                padding={"5px"}
+                fontWeight={"bold"}
+                mt={1}
+                fontFamily={"Nunito Sans, sans-serif"}
               >
-                <img
-                  style={{
-                    height: 260,
-                    maxWidth: 280,
-                    width: "100%",
-                  }}
-                  src={item.Image}
-                  alt="products"
-                />
-                <Typography
-                  fontSize={"13px"}
-                  padding={"5px"}
-                  fontWeight={"bold"}
-                  mt={1}
-                  fontFamily={"Nunito Sans, sans-serif"}
-                >
-                  {item.name}
-                </Typography>
-              </Box>
-            </Link>
+                {item.name}
+              </Typography>
+            </Box>
           </Grid>
         ))}
       </Grid>
