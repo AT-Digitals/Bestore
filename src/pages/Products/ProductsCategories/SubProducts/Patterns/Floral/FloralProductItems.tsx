56719cbe76acb4852c5ab0185821ed173fcd2b32
Dinesh <djeeva1515@gmail.com>
2023-12-20 16:23:28 +0530

    patterns floral changes

---
+++ src/pages/Products/ProductsCategories/SubProducts/Patterns/Floral/FloralProductItems.tsx
@@ -0,0 +1,25 @@
+import Big from "./FloralAssets/BINDU FLOWER.jpg";
+import Big2 from "./FloralAssets/big2.jpg";
+import Bindu from "./FloralAssets/big.jpg";
+import Bindu2 from "./FloralAssets/bindu2.jpg";
+
+const FloralProductItems = [
+  {
+    name: "Bindu Flower",
+    image: Bindu,
+  },
+
+  {
+    name: "Big Flower",
+    image: Big,
+  },
+  {
+    name: "Pattern3",
+    image: Bindu2,
+  },
+  {
+    name: "Pattern4",
+    image: Big2,
+  },
+];
+export default FloralProductItems;
